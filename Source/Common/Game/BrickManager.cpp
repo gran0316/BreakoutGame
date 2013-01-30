14df9654595cbd042bf3de0d4da6784a841fe046
Derek Grant <gran0316@algonquinlive.com>
2013-01-30 16:51:57 -0500

    Trial.
    Tried this and failed epicly.

--- Source/Common/Game/BrickManager.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  BrickManager.cpp
-//  GAM-1532 OSX Game
-//
-//  Created by Derek Grant on 2013-01-30.
-//  Copyright (c) 2013 Algonquin College. All rights reserved.
-//
-
-#include "BrickManager.h"
