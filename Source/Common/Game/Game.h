4b71e7fe6c6db6f78f6a1f6182b81e457e263d36
Derek Grant <gran0316@algonquinlive.com>
2013-03-16 16:28:09 -0400

    Ignoring this

--- Source/Common/Game/Game.h
+++ Source/Common/Game/Game.h
@@ -19,6 +19,9 @@ public:
   void update(double delta);
   void paint();
   void reset();
+    
+  void resetBall();
+  void resetLevel();
   
   //Game Over method, call this when to end the game
   void gameOver();
@@ -34,64 +37,40 @@ public:
   void addGameObject(GameObject* gameObject);
   GameObject* getGameObjectByType(const char* type);
     
-  //Load the game bricks on startup so they exist and can be changed.
-  void loadBricks();
-    
-  //Game level Methods
-    
-  //Loads the current level of the game.
-  void loadGameLevel(int level);
-    
-  //Checks to see if the game is over.
-  bool checkGameOver();
-    
-  //Checks to see if we need a new level.
-  bool newLevel();
-    
-  //Manages the balls on each update to handle active and inactive balls. 
-  void manageBalls();
-    
-  //Checks the current in game ball count.
-  int checkBallCount();
-  
-  //Check for an extra ball proc.
-  void extraBallProc();
-  
-  //Returns a bool wether we should proc extra balls or not.
-  bool getExtraBall();
-  
-  //Spawns balls on proc.
-  void spawnBalls();
-    
 private:
   //Mouse Events
   void mouseMovementEvent(float deltaX, float deltaY, float positionX, float positionY);
 
   //Key Events
   void keyUpEvent(int keyCode);
+    
+  void nextLevel();
+    
+  void paintLevelInfo(float alpha);
 
   //Vector to hold the GameObjects
   std::vector<GameObject*> m_GameObjects;
-  //Vector to manage bricks.
-  std::vector<Brick*> m_CurrentBricks;
-  
-  //Timer variable to delay reseting the game has ended
+
+  //Timer variable to delay reseting the game has ended.
   double m_GameOverTimer;
     
+  //Timer variable to delay the begining for the next level.
+  double m_NextLevelTimer;
+    
+  double m_StartDelayTimer;
+  float m_LevelInfoAlpha;
+    
   //Current game lives.
-  int m_GameLives;
+  short m_GameLives;
    
   //Current game level.
-  int m_CurrentGameLevel;
+  short m_CurrentGameLevel;
    
   //Bool for AI toggle.
   bool m_AI;
     
-  //Bool for extra ball proc.
-  bool m_ExtraBalls;
-    
-  //Ball point to set speed of balls and is used for AI.
-  Ball* m_CurrentActiveBall;
+  //Ball pointer for AI.
+  Ball* m_ActiveBall;
    
   //OpenGLTextures for the game. It's Community themed. It returns February 7th. BE EXCITED!
   OpenGLTexture * m_Background;
